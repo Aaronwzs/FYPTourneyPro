ed7575427bd81db4022fa45567eedb53ed045419
Aaronwzs <zyisyen.wong@gmail.com>
2024-12-02 19:53:09 +0800

    Added Custom Input For ABP User Register

---
+++ FYPTourneyPro/Entities/User/User.cs
@@ -0,0 +1,15 @@
+﻿using Volo.Abp.Identity;
+using System;
+using Volo.Abp.Domain.Entities;
+
+namespace FYPTourneyPro.Entities.User
+{
+    public class User : Entity<Guid>
+    {
+        public Guid UserId { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Nationality { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ FYPTourneyPro/Migrations/20241129180151_AddCustomUserFields.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FYPTourneyPro.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomUserFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    Nationality = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
